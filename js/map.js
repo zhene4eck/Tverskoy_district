16d5c6ab4b459c4ca47b521618f2a1ed71af833d
zhene4eck <eu.rasskazov@gmail.com>
2026-06-08 14:43:50 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,40 +1,78 @@
+// Создание карты
 const map = L.map('map').setView(
     [55.765, 37.605],
     14
 );
 
+// Подложка OpenStreetMap
 L.tileLayer(
     'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
-        attribution: '© OpenStreetMap'
+        attribution: '&copy; OpenStreetMap'
     }
 ).addTo(map);
 
+// Загрузка объектов культурного наследия
 fetch('36.geojson')
     .then(response => response.json())
     .then(data => {
 
         const heritageLayer = L.geoJSON(data, {
 
+            pointToLayer: function(feature, latlng) {
+
+                return L.circleMarker(latlng, {
+                    radius: 6,
+                    color: '#8b0000',
+                    weight: 1,
+                    fillColor: '#c0392b',
+                    fillOpacity: 0.8
+                });
+
+            },
+
             onEachFeature: function(feature, layer) {
 
                 const p = feature.properties;
 
                 layer.bindPopup(`
-                    <div>
-                        <h3>${p.Name}</h3>
-                        <p><b>Категория:</b> ${p.Subtype}</p>
-                        <p><b>Адрес:</b> ${p.Addresses}</p>
+                    <div style="max-width:320px;">
+                        <h3 style="margin-top:0;">
+                            ${p.Name || 'Без названия'}
+                        </h3>
+
+                        <p>
+                            <b>Категория:</b><br>
+                            ${p.Subtype || '-'}
+                        </p>
+
+                        <p>
+                            <b>Вид объекта:</b><br>
+                            ${p.Subsubtype || '-'}
+                        </p>
+
+                        <p>
+                            <b>Адрес:</b><br>
+                            ${p.Addresses || '-'}
+                        </p>
                     </div>
                 `);
+
             }
 
         });
 
         heritageLayer.addTo(map);
 
+        // Автоматический переход к объектам
         map.fitBounds(
-            heritageLayer.getBounds()
+            heritageLayer.getBounds(),
+            {
+                padding: [20, 20]
+            }
         );
 
+    })
+    .catch(error => {
+        console.error('Ошибка загрузки GeoJSON:', error);
     });
