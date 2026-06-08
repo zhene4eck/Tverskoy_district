4cea2337fe3c20d783b182fce88c23bc3434236f
zhene4eck <eu.rasskazov@gmail.com>
2026-06-08 14:14:58 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -16,27 +16,17 @@ fetch('36.geojson')
 
         const heritageLayer = L.geoJSON(data, {
 
-            pointToLayer: function(feature, latlng) {
-                return L.circleMarker(latlng, {
-                    radius: 6,
-                    weight: 1,
-                    fillOpacity: 0.8
-                });
-            },
-
             onEachFeature: function(feature, layer) {
 
                 const p = feature.properties;
 
                 layer.bindPopup(`
-                    <div style="max-width:300px;">
-                        <h3>${p.Full_Name}</h3>
+                    <div>
+                        <h3>${p.Name}</h3>
                         <p><b>Категория:</b> ${p.Subtype}</p>
-                        <p><b>Вид:</b> ${p.Subsubtype}</p>
                         <p><b>Адрес:</b> ${p.Addresses}</p>
                     </div>
                 `);
-
             }
 
         });
